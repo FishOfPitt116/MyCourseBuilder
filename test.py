50c7c673d892a6876e1dcfedc3f89ca0d8fbaaea
Kenny Zhang <mw72889@gmail.com>
2022-02-25 22:03:01 -0500

    Continue testing of section details

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from pittapi import course
 
-courses = course.get_course_sections("2231", "MATH", "1530")
-print("\n\n")
-for section in courses.sections:
-    try:
-        print(section.instructor)
-    except:
-        pass
+CScourses = (course.get_courses("2231", "CS"))
+cs_0007 = CScourses.courses['0007']
+sectionDet = course.get_extra_section_details(section=cs_0007.sections[0] , term="2231", class_number="0007")
+#sectionDet = course.get_section_details('2231', cs_0007.sections[0].class_number)
+print(sectionDet)
+#print(cs_0007)
