dd22a35731e6a38c2fbf56c16ff175b2bab70493
John Doe <mlh184@pitt.edu>
2022-02-25 23:42:05 -0500

    On our way

--- trial1.py
+++ trial1.py
@@ -1,7 +1,9 @@
 from pittapi import course
 
+
+
 class Node:
-	def __init__(self):
+	def __init__(self): # default constructor: Node()
 		self.dept_code = ""# str
 		self.course_no = "" # str
 		self.course_name = "" # str
@@ -9,9 +11,37 @@ class Node:
 		self.preqs = None # List[(Node, bool)]
 		self.completed = False # bool
 	
-	def __init__(self, course.obj):
+	def __init__(self, course_obj): # overload constructor: Node(course_obj)
 		self.dept_code = course_obj.subject_code
-		self.course_no = course.obj.course_number
-		self.course_title = course.obj.course_title
+		self.course_no = course_obj.course_number
+		self.course_title = course_obj.course_title
 	
 		self.completed = False
+		
+	def dept_Code(code, class_type, class_number): 
+		courseType = (course.get_courses(code, class_type)))
+		self.course_no = class_number
+		specific_course = courseType.courses[class_number]
+
+
+def fillDict(term, dept_code):
+	dict = {} 
+	for num, obj in course.get_courses(term, dept_code).course.items():
+		dict[num] = Node(obj)
+	return dict
+
+pitt_object = Node()
+
+def sectionDescriptions(section, term, class_number):
+	sectionDet = course.get_extra_section_details(section, term, class_number)
+	Node.description = sectionDet.description
+
+anyCourse = (course.get_courses("2231", "CS")).courses['0007']
+
+print(pitt_object(sectionDescriptions(anyCourse.sections[0], '2231', '0007')))
+for i in dict: 
+
+# sectionDet = SectionDetails(NamedTuple)
+	
+
+
