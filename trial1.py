460fbbd31df5dddc5bc927cc15015289f70ea08c
Robbie Fishel <robbie.fishel6@gmail.com>
2022-02-26 01:18:39 -0500

    Started work on Graph & adjacency list implementations of prereqs

--- trial1.py
+++ trial1.py
@@ -13,19 +13,18 @@ class Node:
 		self.dept_code = course_obj.subject_code
 		self.course_no = course_obj.course_number
 		self.course_title = course_obj.course_title
-		section_details = course.get_extra_section_details(section=course_obj.sections[0], term=course_obj.sections[0].term, class_number=self.course_no)
-		self.description = section_details.description
-
+		self.section_details = course.get_extra_section_details(section=course_obj.sections[0], term=course_obj.sections[0].term, class_number=self.course_no)
+		self.description = self.section_details.description
 		self.completed = False
 
 	def __str__(self):
 		return self.dept_code + self.course_no + self.course_title + self.description
 
 		
-	def dept_Code(code, class_type, class_number): 
-		courseType = (course.get_courses(code, class_type))
-		self.course_no = class_number
-		specific_course = courseType.courses[class_number]
+	# def dept_Code(code, class_type, class_number): 
+	# 	courseType = (course.get_courses(code, class_type))
+	# 	self.course_no = class_number
+	# 	specific_course = courseType.courses[class_number]
 
 
 def fillDict(term, dept_code):
@@ -36,5 +35,11 @@ def fillDict(term, dept_code):
 		dict[num] = Node(obj)
 		print(str(dict[num]))
 	return dict
+
+class Graph:
+	def __init__(self):
+
+class AdjacencyList:
+	def __init__(self):
 
 fillDict('2231', 'CS')
